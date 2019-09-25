b7ea1ef7ced4ebfc6805df36dcfb60b759c0c64e
Denis Zubarev <zubarev@isa.ru>
2019-09-25 13:46:56 +0000

    extend filtratus

--- ex_pylp/filtratus.py
+++ ex_pylp/filtratus.py
@@ -3,12 +3,19 @@
 
 from ex_pylp.common import Attr
 from ex_pylp.common import PosTag
+from ex_pylp.common import Lang
 from ex_pylp.common import SyntLink
+
+class FiltratusContext:
+    def __init__(self, doc):
+        self.word_strings = doc['words']
+        self.word_ids = doc['word_ids']
+        self.doc_lang = doc['lang']
 
 class Filtratus:
     """http://www.filtratus.com
     """
-    def _filter(self, word_obj, word_pos, sent, word_strings):
+    def _filter(self, word_obj, word_pos, sent, ctx):
         raise NotImplementedError ("!")
 
     def _adjust_syntax_links(self, new_sent, old_sent, new_positions):
@@ -49,12 +56,13 @@ class Filtratus:
 
     def __call__(self, doc_obj):
         new_sents = []
+        ctx = FiltratusContext(doc_obj)
         for sent in doc_obj['sents']:
             new_sent = []
             new_positions = []
             cur_pos = 0
             for word_pos, word_obj in enumerate(sent):
-                if self._filter(word_obj, word_pos, sent, doc_obj['words']):
+                if self._filter(word_obj, word_pos, sent, ctx):
                     new_positions.append(-1)
                 else:
                     new_sent.append(word_obj)
@@ -68,31 +76,84 @@ class Filtratus:
 
 
 class PunctAndUndefFiltratus(Filtratus):
-    def _filter(self, word_obj, word_pos, sent, word_strings):
+    def _filter(self, word_obj, word_pos, sent, ctx):
         if word_obj.get(Attr.POS_TAG, PosTag.UNDEF) in (PosTag.UNDEF, PosTag.PUNCT) and \
            word_obj.get(Attr.SYNTAX_LINK_NAME, SyntLink.PUNCT) == SyntLink.PUNCT:
             return True
         return False
 
 class DeterminatusFiltratus(PunctAndUndefFiltratus):
-    def _filter(self, word_obj, word_pos, sent, word_strings):
-        filtered = super(DeterminatusFiltratus, self)._filter(word_obj, word_pos,
-                                                              sent, word_strings)
+    def _filter(self, word_obj, word_pos, sent, ctx):
+        filtered = super(DeterminatusFiltratus, self)._filter(word_obj, word_pos, sent, ctx)
         if filtered:
             return True
         if word_obj[Attr.POS_TAG] == PosTag.DET:
             return True
         return False
 
-class StopWordsFiltratus(PunctAndUndefFiltratus):
-    def _filter(self, word_obj, word_pos, sent, word_strings):
-        filtered = super(StopWordsFiltratus, self)._filter(word_obj, word_pos, sent, word_strings)
+class StopWordsFiltratus(DeterminatusFiltratus):
+    def __init__(self, stop_words_list_path = ''):
+        if stop_words_list_path:
+            with open(stop_words_list_path, 'r') as f:
+                self._stop_words = frozenset(int(l.strip(), 16) for l in f)
+        else:
+            self._stop_words = []
+
+        self._sw_POSes = frozenset([PosTag.ADP, PosTag.CONJ, PosTag.PART, PosTag.PRON,
+                                    PosTag.AUX, PosTag.SCONJ, PosTag.CCONJ, PosTag.SYM])
+
+
+    def _filter(self, word_obj, word_pos, sent, ctx):
+        filtered = super(StopWordsFiltratus, self)._filter(word_obj, word_pos, sent, ctx)
         if filtered:
             return True
-        #TODO filter CONJ with link cc ?
-        #TODO filter words with links case, obl, fixed, mark, det?
+
+        if word_obj.get(Attr.STOP_WORD_TYPE, 0) > 0:
+            return True
+
+        if word_obj[Attr.POS_TAG] in self._sw_POSes:
+            return True
+
+        if self._stop_words and \
+           ctx.word_ids[word_obj[Attr.WORD_NUM]] in self._stop_words:
+            return True
 
 
-def create_filtratus():
-    #TODO factory
-    return DeterminatusFiltratus()
+        return False
+
+class NumAndUndefLangFiltratus(StopWordsFiltratus):
+    def _filter(self, word_obj, word_pos, sent, ctx):
+        filtered = super(NumAndUndefLangFiltratus, self)._filter(word_obj, word_pos, sent, ctx)
+        if filtered:
+            return True
+
+        word_lang = word_obj.get(Attr.LANG, ctx.doc_lang)
+        if word_lang == Lang.UNDEF:
+            return True
+
+        #by aot
+        # PosTag.NUM  - имя числительное,   PosTag.NUMBER - число
+        if word_lang == Lang.RU:
+            if word_obj[Attr.POS_TAG] == PosTag.NUMBER:
+                return True
+        else:
+            #but by UD PosTag.NUM is any number? and PosTag.NUMBER is not used
+            if word_obj[Attr.POS_TAG] == PosTag.NUM:
+                return True
+
+        return False
+
+
+def create_filtratus(kind = '', **kwargs):
+    if not kind:
+        return DeterminatusFiltratus(**kwargs)
+    if kind == 'punct':
+        return PunctAndUndefFiltratus(**kwargs)
+    if kind == 'determiner':
+        return DeterminatusFiltratus(**kwargs)
+    if kind == 'stopwords':
+        return StopWordsFiltratus(**kwargs)
+    if kind == 'num&undeflang':
+        return NumAndUndefLangFiltratus(**kwargs)
+
+    raise RuntimeError("Unknown filtratus kind: %s" % kind)
